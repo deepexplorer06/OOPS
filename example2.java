9682d78aa80c071dacaf89e0bc585e9ddee2de68
deepexplorer06 <sathwikraoarrabelly@gmail.com>
2025-03-02 23:59:51 +0530

    Add files via upload

---
+++ example2.java
@@ -0,0 +1,9 @@
+class example2 {
+	public static void main(String[] args){
+		System.out.println("Name: A Sathwik Rao");
+		System.out.println("Roll No: 24009");
+		System.out.println("Section: CSE A");
+
+}
+
+}
